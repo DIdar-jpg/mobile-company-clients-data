30555476881f30d2c4f59e829ff5a2b05053ec31
Didar-jpg <guneshev.didar@gmail.com>
2025-08-02 15:59:57 +0500

    Changed viteConfig.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
    },
 
    plugins: [react(), tailwindcss()],
+   base: 'mobile-company-clients-data',
    resolve: {
       alias: {
          "@": path.resolve(__dirname, "./src"),
